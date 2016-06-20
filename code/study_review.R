6f1b4cd4e0b662cae4a70f70bc32e0ab14dc6f88
James Lawson <the.hierarchy@gmail.com>
2016-06-20 17:12:39 +1000

    Working on diagnostic plots for study reviews #50
    pairwise_panel() and dotcharts() don't work in all cases yet

---
+++ code/study_review.R
@@ -0,0 +1,150 @@
+require(plyr)
+require(readr)
+require(remake)
+require(reshape2)
+
+#setwd('../')
+austraits <- remake::make('austraits')
+
+# calculates coefficient of variation
+CV <- function(x){
+  sqrt(var(x))/mean(x)
+}
+
+#########
+
+## DIAGNOSTIC PLOTS
+
+format_data <- function(id) { 
+
+  id <- deparse(substitute(id))
+  study_data <- subset(austraits$data, study == id)
+  study_metadata <- subset(austraits$metadata, dataset_id == id)
+  study_data <- study_data[!is.na(study_data$value),]
+  
+  definitions_traits_numeric <- subset(read_csv('config/definitions_traits.csv'), type == "numeric")
+  
+  data_all <- austraits$data
+  
+  study_traits <- unique(study_data[study_data$trait_name %in% unique(definitions_traits_numeric$trait_name),]$trait_name) # numeric traits from our target study please
+  study_traits_alldata <- data_all[data_all$trait_name %in% study_traits,] # and pull out all austraits data for those traits
+  study_traits_alldata <- study_traits_alldata[!is.na(study_traits_alldata$value),]
+  
+  #study_traits_alldata$seq <- with(study_traits_alldata, ave(value, species_name, trait_name, study, FUN = seq_along)) # adds a sequence number for multiple values within datasets so I can cast long to wide without aggregating
+  #study_traits_alldata_wide <- dcast(study_traits_alldata, seq + study + species_name ~ trait_name, value.var = 'value', fun.aggregate=function(x) as.character(x)[1])
+  
+  # aggregate to mean vals per species/trait across all data
+  study_traits_alldata <- study_traits_alldata[!study_traits_alldata$study == id,]  # remove target dataset
+  study_traits_alldata$value <- as.numeric(study_traits_alldata$value)
+  
+  study_traits_alldata <- ddply(study_traits_alldata, .(species_name, trait_name, unit), summarise, 
+                                trait_mean = mean(value), trait_CV = CV(value))
+  study_traits_alldata_wide <- dcast(study_traits_alldata, species_name ~ trait_name, value.var = 'trait_mean', fun.aggregate=function(x) paste(x, collapse = ", "))
+  study_traits_alldata_wide$target <- 'all data'
+  
+  
+  # add in non-aggregated data from target study
+  study_data_numeric <- study_data[study_data$trait_name %in% unique(definitions_traits_numeric$trait_name),]
+  study_data_numeric$seq <- with(study_data_numeric, ave(value, species_name, trait_name, study, FUN = seq_along)) #adds a sequence number for multiple values within datasets so I can cast long to wide without aggregating
+  study_data_numeric_wide <- dcast(study_data_numeric, study + seq + species_name ~ trait_name, value.var = 'value', fun.aggregate=function(x) as.character(x)[1])
+  study_data_numeric_wide$seq <- NULL
+  study_data_numeric_wide$study <- NULL
+  study_data_numeric_wide$target <- paste(id)
+  
+  # put it all together
+  all <- rbind(study_data_numeric_wide,study_traits_alldata_wide)
+  #all[,2:(ncol(all)-1)] <- lapply(all[,2:(ncol(all)-1)], as.numeric)
+
+  if(ncol(all) > 3) { # lapply breaks if there's only one column to apply a function to
+    all[,2:(ncol(all)-1)] <- lapply(all[,2:(ncol(all)-1)], as.numeric)
+  } else {
+  #  all[,2:(ncol(all)-1)] <- as.numeric(all[,2:(ncol(all)-1)])
+    all[,2] <- as.numeric(all[,2])
+  }
+
+  return(all)
+   
+}
+
+
+pairwise_panel <- function(id) {
+
+    id <- deparse(substitute(id))
+    
+    panel.hist <- function(x, ...)
+    {
+      usr <- par("usr"); on.exit(par(usr))
+      par(usr = c(usr[1:2], 0, 1.5) )
+      h <- hist(x, plot = FALSE)
+      breaks <- h$breaks; nB <- length(breaks)
+      y <- h$counts; y <- y/max(y)
+      rect(breaks[-nB], 0, breaks[-1], y, col = "cyan", ...)
+    }
+    
+    col.rainbow <- rainbow(2:3)
+    palette(col.rainbow)
+  
+    pairs(log10(all[,2:(ncol(all)-1)]), panel = panel.smooth,
+          cex = 1, pch = 24, bg = all$target,
+          diag.panel = panel.hist, cex.labels = 1, font.labels = 1,
+          main = paste('Pairwise plots for', id, sep = " "))
+  
+}  
+
+
+pairwise_panel <- function(id) {
+  
+  id <- deparse(substitute(id))
+  
+  panel.hist <- function(x, ...)
+  {
+    usr <- par("usr"); on.exit(par(usr))
+    par(usr = c(usr[1:2], 0, 1.5) )
+    h <- hist(x, plot = FALSE)
+    breaks <- h$breaks; nB <- length(breaks)
+    y <- h$counts; y <- y/max(y)
+    rect(breaks[-nB], 0, breaks[-1], y, col = "cyan", ...)
+  }
+  
+  col.rainbow <- rainbow(2:3)
+  palette(col.rainbow)
+  
+  pairs(log10(all[,2:(ncol(all)-1)]), panel = panel.smooth,
+        cex = 1, pch = 24, bg = all$target,
+        diag.panel = panel.hist, cex.labels = 1, font.labels = 1,
+        main = paste('Pairwise plots for', id, sep = " "))
+  
+}  
+
+ 
+dotcharts <- function(id) {
+  
+  id <- deparse(substitute(id))
+ 
+  panel_dims <- round(sqrt(length(all[,2:(ncol(all)-1)])), digits = 0)
+  
+  col.rainbow <- rainbow(2:3)
+  palette(col.rainbow)
+  
+  par(mfrow=c(panel_dims,panel_dims),
+      oma = c(2,1,0,1) + 0.1,
+      mar = c(2,1,2,1) + 0.1,
+      bg = 'white')
+  
+  for(i in 2:(ncol(all)-1)) {
+    dotchart(log10(all[,i]), groups = all$target, color = all$target, main = colnames(all)[i], lcolor = 'white')
+  }
+
+}
+
+
+all <- format_data(dataset_013)
+pairwise_panel(dataset_013)
+dotcharts(dataset_013
+
+
+
+
+
+
+
