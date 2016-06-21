e7e212e9928095946a67e626cf7ddfe11ecfaea4
Daniel Falster <daniel.falster@mq.edu.au>
2016-06-21 11:43:02 +1000

    Skeleton for generating reports for all studies
    Towards #50, #31

--- bootstrap.R
+++ bootstrap.R
@@ -7,5 +7,5 @@ vals <- list(study_names=iteratelist(study_names, value="study_name"))
 str <- whisker.render(readLines("config/remake_data.yml.whisker"), vals)
 writeLines(str, "remake_data.yml")
 
-# str <- whisker.render(readLines("config/remake_reports.yml.whisker"), vals)
-# writeLines(str, "remake_reports.yml")
+str <- whisker.render(readLines("config/remake_reports.yml.whisker"), vals)
+writeLines(str, "remake_reports.yml")
