953355e917f8956a33ff6c0f08137ecc1810afc9
Elizabeth Wenk <ehwenk@gmail.com>
2019-09-20 09:28:04 +1000

    Lim_2017_ changes to close #234
    * fixing issues with substitutions to allow different data files to merge

--- data/Lim_2017/more code to merge data files.R
+++ data/Lim_2017/more code to merge data files.R
@@ -1,9 +1,9 @@
 read_csv("data/Lim_2017/raw/Lim_Bogong_leafTraits_LeafArea_Dryweight.csv") %>%
+  mutate(Site = Site %>% str_replace("FBPN","BPN")) %>%
   group_by(Date, Site, Position, Spp, Branchno, `Date dryweight`) %>%
   summarise(Area_sum = sum(Area),leaf_count = max(IndividualLeafnumber)) -> leaf_area_sum
 
 read_csv("data/Lim_2017/raw/Lim_Bogong_leafTraits_LeafArea_Dryweight.csv") %>%
-  mutate(Site = gsub("FBPN","BPN",Site)) %>%
   filter(!is.na(DryweightGroup)) %>%
   select(Site,Position,Spp,Branchno,DryweightGroup) %>%
   left_join(leaf_area_sum,by = (c("Site", "Position", "Spp", "Branchno"))) %>%
@@ -15,12 +15,13 @@ library("rqdatatable")
 install.packages("rqdatatable")
 Lim_check <- 
 read_csv("data/Lim_2017/data.csv") %>%
-  mutate(Spp = gsub("Kunzea muelleri","KuMue",Spp),
-         Site = gsub("F04North","F04N",Site)) %>%
+  mutate(Spp = ifelse(Spp == "Kunzea muelleri","KuMue",Spp),
+         Site = ifelse(Site == "F04North","F04N",Site),
+         Site = ifelse(Site == "BPN","BPN",Site)) %>%
   natural_join(leaf_area_sum2, by = (c("Site", "Position", "Spp", "Branchno")), jointype = "FULL") %>%
-  mutate(Spp = gsub("KuMue","Kunzea muelleri",Species),
-         Spp = gsub("BaGun","Baeckea gunniana",Species),
-         Spp = gsub("OlFro","Olearia frostii",Species)) -> data_tmp
+  mutate(Species = ifelse(Spp == "KuMue","Kunzea muelleri",Species),
+         Species = ifelse(Spp == "BaGun","Baeckea gunniana",Species),
+         Species = ifelse(Spp == "OlFro","Olearia frostii",Species)) -> data_tmp
 
 write_csv(data_tmp,"data/Lim_2017/data.csv")
 
