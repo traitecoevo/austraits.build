071f9d909b2d8f946bd980266151822c17ee3afd
Elizabeth Wenk <ehwenk@gmail.com>
2019-10-16 20:43:37 +1100

    Kew_2019_6: add study, metadata

--- data/Kew_2019_6/code to manipulate germination data.R
+++ data/Kew_2019_6/code to manipulate germination data.R
@@ -1,3 +1,5 @@
+read_csv("data/Kew_2019_6/raw/match_terms_Kew_2019_06.csv") -> match
+
 read_csv("data/Kew_2019_6/raw/Liu_Kew_germination.csv", guess_max = 10000) %>%
   
   #extract numbers are beginning of cell
@@ -6,8 +8,9 @@ read_csv("data/Kew_2019_6/raw/Liu_Kew_germination.csv", guess_max = 10000) %>%
   #extract numbers at end of cell
   mutate(seeds_sown = as.numeric(str_match(percentgerm,"(?<=sown\\: )[0-9]+"))) %>%
   select(-percentgerm) %>%
-  write_csv("data/Kew_2019_6/data.csv")
-  
+  left_join(match,by = "presowtreat") %>% 
+  write_csv("data/Kew_2019_6/data.csv") 
+
 
 #extract numbers are beginning of cell
 data$percentgerm %>% str_sub(1,3) %>% as.numeric()
