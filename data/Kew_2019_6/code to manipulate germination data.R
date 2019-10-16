e252a2cadae2e99d313b702f0e6285f178a20677
Elizabeth Wenk <ehwenk@gmail.com>
2019-10-16 12:14:31 +1100

    Kew_2019_06: manipulations to raw data file

---
+++ data/Kew_2019_6/code to manipulate germination data.R
@@ -0,0 +1,16 @@
+read_csv("data/Kew_2019_6/raw/Liu_Kew_germination.csv", guess_max = 10000) %>%
+  
+  #extract numbers are beginning of cell
+  mutate(germination_percent = as.numeric(str_sub(percentgerm,1,3))) %>%
+  
+  #extract numbers at end of cell
+  mutate(seeds_sown = as.numeric(str_match(percentgerm,"(?<=sown\\: )[0-9]+"))) %>%
+  select(-percentgerm) %>%
+  write_csv("data/Kew_2019_6/data.csv")
+  
+
+#extract numbers are beginning of cell
+data$percentgerm %>% str_sub(1,3) %>% as.numeric()
+
+#extract numbers at end of cell
+data$percentgerm %>% str_match("(?<=sown\\: )[0-9]+") %>% as.numeric()
