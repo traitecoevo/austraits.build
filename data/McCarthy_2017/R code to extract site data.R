3d980469fa11aac5b833f0f71a2b65d60466cf68
Elizabeth Wenk <ehwenk@gmail.com>
2019-10-16 18:06:48 +1100

    McCarthy_2017: part of previous commit

--- data/McCarthy_2017/R code to extract site data.R
+++ data/McCarthy_2017/R code to extract site data.R
@@ -1,9 +1,6 @@
 read_csv("data/McCarthy_2017/raw/data_JKMupdate_20191016.csv") %>%
-  select(Site,Latitude,Longitude,`Coordinate uncertainty (m)`) %>%
-  rename(`trip identifier`=Site,`latitude (deg)` = Latitude,`longitude (deg)`= Longitude) %>%
-  mutate(site_name = ifelse(is.na(`latitude (deg)),`trip identifier`,
+  rename(`trip identifier`= Site,`latitude (deg)` = Latitude,`longitude (deg)`= Longitude) %>%
+  mutate(site_name = ifelse(is.na(`latitude (deg)`),`trip identifier`,
                             paste("site_at",`latitude (deg)`,"degS",`longitude (deg)`,"degE",sep="_"))) %>%
-  distinct(site_name, .keep_all = TRUE) %>%
-  write_csv("data/McCarthy_2017/data.csv") -> sites
-
-View(sites)
+  write_csv("data/McCarthy_2017/data.csv") %>%
+  distinct(site_name, .keep_all = TRUE) -> sites
