e536874320e761222219386ac9dacb0e4274ebf0
Elizabeth Wenk <ehwenk@gmail.com>
2019-11-08 09:15:47 +1100

    Moore_2019: update site names
    * updated some site names (previously numbers) with location names
    * other final small edits

--- data/Moore_2019/code to extract site data and manipulate species names.R
+++ data/Moore_2019/code to extract site data and manipulate species names.R
@@ -1,4 +1,6 @@
-read_csv("data/Moore_2006/data.csv") %>%
+read_csv("data/Moore_2019/site_names.csv") -> site_names
+
+read_csv("data/Moore_2019/data.csv") %>%
   select('site_tree_code', 'site_Longitude', 'site_Latitude', 'soil_TOC', 'soil_pH', 'soil_conductivity', 'soil_N', 'soil_P', 
          'soil_K', 'soil_Sand', 'soil_Silt', 'soil_Clay', 'site_elevation') %>%
   mutate(site_name = trunc(site_tree_code,0)) %>%
@@ -7,12 +9,14 @@ read_csv("data/Moore_2006/data.csv") %>%
   group_by(site_name) %>%
   distinct() %>%
   rename(`longitude (deg)` = `site_Longitude`,`latitude (deg)` = `site_Latitude`) %>%
-  write_csv("data/Moore_2006/site_data.csv")
+  left_join(site_names, by="site_name") %>%
+  write_csv("data/Moore_2019/site_data.csv")
 
-read_csv("data/Moore_2006/site_data.csv") -> Moore_sites
+read_csv("data/Moore_2019/site_data.csv") -> Moore_sites
 
-read_csv("data/Moore_2006/data.csv") %>%
+read_csv("data/Moore_2019/data.csv") %>%
   mutate(site_name = trunc(site_tree_code,0)) %>%
   mutate(site_name = paste0("site_",site_name)) %>%
   mutate(taxon = paste(Genus,species,sep=" ")) %>%
-  write_csv("data/Moore_2006/data.csv")
+  left_join(site_names, by="site_name") %>%
+  write_csv("data/Moore_2019/data.csv")
