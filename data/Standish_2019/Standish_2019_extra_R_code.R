f39e1f0b0fe1e3e4de965300d2aaa25b082f6871
Elizabeth Wenk <ehwenk@gmail.com>
2019-11-18 12:40:59 +1100

    Standish_2019: finish entering study
    * all taxa known
    * no errors
    * AusTraits builds
    * report ready to send to contributor

--- data/Standish_2019/Standish_2019_extra_R_code.R
+++ data/Standish_2019/Standish_2019_extra_R_code.R
@@ -1,7 +1,10 @@
 read_csv("data/Standish_2019/Standish_2019.csv") %>%
   mutate(`Seed mass (mg)` = ifelse(`SM data source`=="Kew SID",NA,`Seed mass (mg)`)) %>%
-  mutate(nitrogen_fixer = ifelse(startsWith(`Nutrient-acquisition strategy`,"Nfixer"),1,0)) %>%
+  mutate(nitrogen_fixing = ifelse(startsWith(`Nutrient-acquisition strategy`,"Nfixer"),1,0)) %>%
   mutate(`Nutrient-acquisition strategy`= gsub("Nfixer/","",`Nutrient-acquisition strategy`)) %>%
+  mutate(life_history = ifelse(`Life Form` %in% c("shrub","tree","perennial forb","perennial grass",
+                                                  "perennial rush","perennial sedge"),"perennial","annual")) %>%  
+  mutate(`Life Form` = gsub("annual ","",`Life Form`)) %>%
+  mutate(`Life Form` = gsub("perennial ","",`Life Form`)) %>%
+  mutate(`Species Name` = gsub("*","",`Species Name`)) %>%
   write_csv("data/Standish_2019/data.csv")
-
-       
