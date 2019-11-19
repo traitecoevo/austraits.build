b204e74874b07365feb497d9df128e26cff859c6
Elizabeth Wenk <ehwenk@gmail.com>
2019-11-19 12:32:42 +1100

    Hayes_2018: import new study * AusTraits builds * no errors * report looks good and ready to send * no explicit questions for contributors

--- data/Hayes_2018/Hayes_2018_extra_R_code.R
+++ data/Hayes_2018/Hayes_2018_extra_R_code.R
@@ -1,22 +1,55 @@
 read_csv("data/Hayes_2018/Hayes_et_al._2018_site.data.csv") %>%
-  select(`site.id`,`date`,`location detail`,`GPS coords`) %>%
+  select(`site.id`,`date`,`location detail`,`latitude (deg)`,`longitude (deg)`) %>%
   group_by(`site.id`) %>%
-  summarise_all(.funs = mean) -> sites_char_info
-
+  summarise_all(.funs = first) -> sites_char_info
 
 read_csv("data/Hayes_2018/Hayes_et_al._2018_site.data.csv") %>%
-select(-`sample #`,-`sample detail`,-`species sampled`,-`replicate #`,-`country`,-`laboratory`) %>%
+select(-`sample #`,-`sample detail`,-`species sampled`,-`replicate #`,-`country`,-`laboratory`,
+       -`latitude (deg)`,-`longitude (deg)`,-`date`,-`location detail`,-`GPS coords`) %>%
 group_by(`site.id`) %>%
-  left_join(sites_char_info,by="sites_char_info") %>%
-  summarise_all(.funs = mean) -> site_data 
+  summarise_all(.funs = mean) %>%
+ungroup() %>%
+left_join(sites_char_info,by="site.id") -> site_data 
 
+read_csv("data/Hayes_2018/Hayes_et_al._2018_leaf.data.csv") %>%
+  mutate(Zn_mg.kg = gsub("<1.0","1.0",Zn_mg.kg)) %>%
+  mutate(Al_mg.kg = gsub("<20","20",Al_mg.kg)) %>%
+  mutate(Zn_mg.kg_numeric = as.numeric(Zn_mg.kg)) %>% 
+  mutate(Al_mg.kg_numeric = as.numeric(Al_mg.kg)) %>%
+  select(-`GPS co-ords`,- `Al_mg.kg`, -`Zn_mg.kg`) %>%
+  rename(sample = `sample code`) -> leaf_data
 
+read_csv("data/Hayes_2018/Hayes_et_al._2018_cell.data.csv") %>%
+  select(-`GPS coords`,-`spectrum.label`) %>%
+  mutate(cell.type = gsub(".Ca","",cell.type),
+         cell.type = gsub("EP","epidermis",cell.type),
+         cell.type = gsub("HY","hypodermis",cell.type),
+         cell.type = gsub("PM","palisade_mesophyll",cell.type),
+         cell.type = gsub("SM","spongy_mesophyll",cell.type),
+         cell.type = gsub("IP","internal_parenchyma",cell.type),
+         cell.type = gsub("SC","sclerenchyma",cell.type)) %>%
+  group_by(site.id, cell.type,species, sample, biological.rep) %>%
+  summarise_all(.funs = mean) %>%
+  ungroup() -> before_spread
 
+before_spread %>%
+  select(-Ca) %>%
+  spread(key = cell.type, value = P) %>%
+  rename(epidermis_P = epidermis, hypodermis_P = hypodermis, palisade_mesophyll_P = palisade_mesophyll,
+         spongy_mesophyll_P = spongy_mesophyll, internal_parenchyma_P = internal_parenchyma, 
+         sclerenchyma_P = sclerenchyma) -> cell_P_data
 
-View(site_data)
+before_spread %>%
+  select(-P) %>%
+  spread(key = cell.type, value = Ca) %>%
+  rename(epidermis_Ca = epidermis, hypodermis_Ca = hypodermis, palisade_mesophyll_Ca = palisade_mesophyll,
+         spongy_mesophyll_Ca = spongy_mesophyll, internal_parenchyma_Ca = internal_parenchyma, 
+         sclerenchyma_Ca = sclerenchyma) %>%
+  select(-species, - site.id, -biological.rep, - `latitude (deg)`, -`longitude (deg)`) %>%
+  left_join(cell_P_data, by = "sample") -> cell_data
 
-group_by()
--> site_data
+cell_data %>%
+  bind_rows(leaf_data) %>%
+  write_csv("data/Hayes_2018/data.csv")
 
-
-111
+View(test)
