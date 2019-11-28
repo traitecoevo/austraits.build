2fad9156ac6594aa47b252d58bc91f57b5b79bb7
Elizabeth Wenk <ehwenk@gmail.com>
2019-11-28 13:48:39 +1100

    Muir_2014: edits to data and metadata
    * removed seed_mass (from Kew)
    * now using species means, because values at individual level were clearly all the same for the species

--- data/Muir_2014/Muir_2014_R_code_to_merge_information.R
+++ data/Muir_2014/Muir_2014_R_code_to_merge_information.R
@@ -3,6 +3,22 @@ read_csv("data/Muir_2014/species_names.csv") -> species_names
 read_csv("data/Muir_2014/raw/EnsembleData.csv") %>%
   subset(Site=="Foothills") %>%
   left_join(species_names, by = "Sp") %>%
+  write_csv("data/Muir_2014/data_individual.csv")
+
+read_csv("data/Muir_2014/flower_counts.csv") %>%
+  select(species_name, flower_count_maximum) -> flower_counts
+
+read_csv("data/Muir_2014/data_individual.csv") %>%
+  select(species_name, Site,site_name, years_since_fire, 
+         `latitude (deg)`, `longitude (deg)`) %>%
+  group_by(species_name) %>%
+  summarise_all(.funs = last) -> sites_to_merge
+
+read_csv("data/Muir_2014/data_individual.csv") %>%
+  select(species_name, SDMASS, SLA, N) %>%
+  group_by(species_name) %>%
+  summarise_all(.funs = first) %>%
+  ungroup() %>%
+  left_join(flower_counts, by = "species_name") %>%
+  left_join(sites_to_merge, by = "species_name") %>%
   write_csv("data/Muir_2014/data.csv")
-
-
