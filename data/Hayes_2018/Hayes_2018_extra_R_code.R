e748be8658278f1e60d28a9b49e38ed3ccdc266e
Elizabeth Wenk <ehwenk@gmail.com>
2019-11-29 12:11:38 +1100

    Hayes_2018: remanipulating data, editing metadata
    * lots of changes based on responses from the data contributor
    * now includes columns indicating how many intra-individual replicates were combined into the values in the data.csv file
    * first round of review by author complete

--- data/Hayes_2018/Hayes_2018_extra_R_code.R
+++ data/Hayes_2018/Hayes_2018_extra_R_code.R
@@ -17,10 +17,11 @@ read_csv("data/Hayes_2018/Hayes_et_al._2018_leaf.data.csv") %>%
   mutate(Zn_mg.kg_numeric = as.numeric(Zn_mg.kg)) %>% 
   mutate(Al_mg.kg_numeric = as.numeric(Al_mg.kg)) %>%
   select(-`GPS co-ords`,- `Al_mg.kg`, -`Zn_mg.kg`) %>%
-  rename(sample = `sample code`) -> leaf_data
+  rename(biological.rep = `replicate`)%>%
+  rename(replicate_individuals = `sample code`) -> leaf_data
 
 read_csv("data/Hayes_2018/Hayes_et_al._2018_cell.data.csv") %>%
-  select(-`GPS coords`,-`spectrum.label`) %>%
+  select(-`GPS coords`,-`spectrum.label`,-`sample`) %>%
   mutate(cell.type = gsub(".Ca","",cell.type),
          cell.type = gsub("EP","epidermis",cell.type),
          cell.type = gsub("HY","hypodermis",cell.type),
@@ -28,28 +29,47 @@ read_csv("data/Hayes_2018/Hayes_et_al._2018_cell.data.csv") %>%
          cell.type = gsub("SM","spongy_mesophyll",cell.type),
          cell.type = gsub("IP","internal_parenchyma",cell.type),
          cell.type = gsub("SC","sclerenchyma",cell.type)) %>%
-  group_by(site.id, cell.type,species, sample, biological.rep) %>%
+  mutate(replicate_individuals = paste(species,biological.rep,sep="_")) -> cell_temp
+
+cell_temp %>%
+  group_by(site.id, cell.type,species, biological.rep, replicate_individuals) %>%
+  summarise(sample_reps = n()) %>%
+  ungroup() -> counts
+  
+cell_temp %>%
+  group_by(site.id, cell.type,species, biological.rep, replicate_individuals) %>%
   summarise_all(.funs = mean) %>%
-  ungroup() -> before_spread
+  ungroup() %>%
+  left_join(counts, by = c("species","replicate_individuals","site.id","cell.type","biological.rep")) -> before_spread
 
 before_spread %>%
-  select(-Ca) %>%
+  select(-Ca, -P) %>%
+  spread(key = cell.type, value = sample_reps) %>%
+  rename(epidermis_counts = epidermis, hypodermis_counts = hypodermis, 
+         palisade_mesophyll_counts = palisade_mesophyll,
+         spongy_mesophyll_counts = spongy_mesophyll, 
+         internal_parenchyma_counts = internal_parenchyma, 
+         sclerenchyma_counts = sclerenchyma) %>%
+  select(-species, -biological.rep, - `latitude (deg)`, -`longitude (deg)`) -> counts_spread
+
+before_spread %>%
+  select(-Ca, -sample_reps) %>%
   spread(key = cell.type, value = P) %>%
   rename(epidermis_P = epidermis, hypodermis_P = hypodermis, palisade_mesophyll_P = palisade_mesophyll,
          spongy_mesophyll_P = spongy_mesophyll, internal_parenchyma_P = internal_parenchyma, 
-         sclerenchyma_P = sclerenchyma) -> cell_P_data
+         sclerenchyma_P = sclerenchyma) %>%
+  select(-species, -biological.rep, - `latitude (deg)`, -`longitude (deg)`) -> cell_P_data
 
 before_spread %>%
-  select(-P) %>%
+  select(-P, -sample_reps) %>%
   spread(key = cell.type, value = Ca) %>%
   rename(epidermis_Ca = epidermis, hypodermis_Ca = hypodermis, palisade_mesophyll_Ca = palisade_mesophyll,
          spongy_mesophyll_Ca = spongy_mesophyll, internal_parenchyma_Ca = internal_parenchyma, 
          sclerenchyma_Ca = sclerenchyma) %>%
-  select(-species, - site.id, -biological.rep, - `latitude (deg)`, -`longitude (deg)`) %>%
-  left_join(cell_P_data, by = "sample") -> cell_data
+  select(- `latitude (deg)`, -`longitude (deg)`) %>%
+  left_join(cell_P_data, by = c("site.id","replicate_individuals")) %>%
+  left_join(counts_spread, by = c("site.id","replicate_individuals")) -> cell_data
 
 cell_data %>%
   bind_rows(leaf_data) %>%
   write_csv("data/Hayes_2018/data.csv")
-
-View(test)
