5933a62880aac00d8b6a26624c11f04a4266984a
Elizabeth Wenk <ehwenk@gmail.com>
2019-12-05 13:20:41 +1100

    Hayes_2018: move raw files to raww folder

--- data/Standish_2019/Standish_2019_extra_R_code.R
+++
@@ -1,7 +0,0 @@
-read_csv("data/Standish_2019/Standish_2019.csv") %>%
-  mutate(`Seed mass (mg)` = ifelse(`SM data source`=="Kew SID",NA,`Seed mass (mg)`)) %>%
-  mutate(nitrogen_fixer = ifelse(startsWith(`Nutrient-acquisition strategy`,"Nfixer"),1,0)) %>%
-  mutate(`Nutrient-acquisition strategy`= gsub("Nfixer/","",`Nutrient-acquisition strategy`)) %>%
-  write_csv("data/Standish_2019/data.csv")
-
-       
