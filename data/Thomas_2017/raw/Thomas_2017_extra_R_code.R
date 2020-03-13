b81b8b1cb6f094542c92d9edcf3c5c5e3ee7f06f
Elizabeth Wenk <ehwenk@gmail.com>
2020-03-13 18:17:53 +1100

    Thomas_2017: reworking study to have plant_height, basa_diameter, plant_width as speies max values, not individual values

--- data/Thomas_2017/raw/Thomas_2017_extra_R_code.R
+++ data/Thomas_2017/raw/Thomas_2017_extra_R_code.R
@@ -5,6 +5,22 @@ read_csv("data/Thomas_2017/raw/FT_species_traits.csv") %>%
   distinct(SPP, .keep_all = TRUE) -> species_mean
 
 read_csv("data/Thomas_2017/raw/FT_species_traits.csv") %>%
+  select(SPP, Yrs, Ht, Length, Width, Diam) %>%
+  group_by(SPP,Yrs) %>%
+  summarise_all(.funs=max) %>%
+  ungroup() -> species_max
+
+#using only max allometric traits
+species_max %>%
+  bind_rows(species_mean) %>%
+  rename(Abbreviation = SPP) %>%
+  left_join(species_names,by = "Abbreviation") %>%
+  mutate(species_binomial = paste(Genus,species,sep=" ")) %>%
+  mutate(site = "MurraySunset") %>%
+  write_csv("data/Thomas_2017/data.csv")
+
+#old version
+read_csv("data/Thomas_2017/raw/FT_species_traits.csv") %>%
   select(-X1, - SLA, -SD, -SD_MASS, -N) %>%
   bind_rows(species_mean) %>%
   rename(Abbreviation = SPP) %>%
