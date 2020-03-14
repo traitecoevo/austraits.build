01d89261c622ab98e8b7298ac8c849819a1ed982
Elizabeth Wenk <ehwenk@gmail.com>
2020-03-14 13:48:17 +1100

    additional work for Pollock_2012

--- data/Pollock_2012/raw/Pollock_2012_extra_R_code.R
+++ data/Pollock_2012/raw/Pollock_2012_extra_R_code.R
@@ -1,9 +1,26 @@
 install.packages("euctraits", repos = "https://r-pkgs.williamkmorris.com")
 library("euctraits")
 
-euctraits::medians_grampians  %>%
-  select(taxon, fruit_mass_mg, fruit_wall_width_mm, fruit_diameter_mm, fruit_height_mm, seed_mass_mg, max_height_m) ->  Grampian_species_medians
+euctraits::traits_grampians -> traits_grampians
 
+euctraits::medians_grampians  %>%
+  select(taxon, max_height_m) ->  Grampian_max_height
+
+#newer version
+read_csv("data/Pollock_2012/raw/grampians_EWenk_edited_latlon.csv", guess_max=10000) %>%
+  subset(is.na(omit)) %>%
+  bind_rows(Grampian_max_height) %>%
+  rename(`longitude (deg)`= longitude_gda94, `latitude (deg)` = latitude_gda94, `elevation (m)` = altitude_m) %>%
+  mutate(notes = ifelse(is.na(`latitude (deg)`),"generic Grampians lat/lon","")) %>%
+  mutate(`latitude (deg)` = ifelse(is.na(`latitude (deg)`),-37.26125,`latitude (deg)`)) %>%
+  mutate(`longitude (deg)` = ifelse(is.na(`longitude (deg)`),142.4448611,`longitude (deg)`)) %>%
+  mutate(`elevation (m)` = ifelse(`elevation (m)` == 0,NA,`elevation (m)`)) %>%
+  write_csv("data/Pollock_2012/data.csv") %>%
+  select(locality_consistent,`longitude (deg)`, `latitude (deg)`, `elevation (m)`, notes, date_time, collector) %>%
+  distinct(locality_consistent, .keep_all = TRUE) %>%
+  write_csv("data/Pollock_2012/raw/site_data2.csv") -> site_data
+
+#old version
 read_csv("data/Pollock_2012/raw/grampians.csv") %>%
   mutate(seed_mass_mg = ifelse(TRUE,NA,seed_mass_mg)) %>%
   mutate(fruit_wall_width_mm = ifelse(TRUE,NA,fruit_wall_width_mm)) %>%
