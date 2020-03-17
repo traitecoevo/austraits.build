c23b28aa18f52f870ad81e86a6be8a01987af330
Elizabeth Wenk <ehwenk@gmail.com>
2020-03-17 13:15:37 +1100

    Sams_2017: input study

--- data/Sams_2017/raw/Sams_2017 extra R code.R
+++ data/Sams_2017/raw/Sams_2017 extra R code.R
@@ -21,4 +21,9 @@ Sams_to_use %>%
          seed_max_mean = maxseedlength, fruit_min_mean = `fruit size(min).mm2`, 
          fruit_max_mean =`fruit size(max).mm2`) %>%
   full_join(Sams_max,by=c("species_name")) %>% 
-  write_csv("data/Sams_2017/raw/test2.csv")
+  write_csv("data/Sams_2017/raw/test2.csv") 
+
+#code to compare Metacalfe_2020_2, Kooyman_2011, Cooper_2013
+subset(austraits$traits,dataset_id %in% c("Sams_2017","Metcalfe_2020_2","Cooper_2013","Kooyman_2011")) %>%
+  subset(trait_name %in% c("wood_density","specific_leaf_area","plant_height","seed_length")) %>% 
+  write_csv("data/Sams_2017/raw/comparing_across_studies.csv")
