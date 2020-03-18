8dd00d6e60c6bb8865dd226e89ef9c59a0a15105
Daniel Falster <daniel.falster@unsw.edu.au>
2020-03-18 17:22:59 +1100

    Explictly include the pipe

--- R/support.R
+++ R/support.R
@@ -1,3 +1,16 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
 #' Swap a null vale to something else
 #'
 #' @param x 
