e30d756765a44d0a067f106364f6ecbe744d5421
Elizabeth Wenk <ehwenk@gmail.com>
2020-04-02 14:00:42 +1100

    Tomlinson_2018: import new study
    * imported new study
    * report builds, no errors, added contexts
    * sent report to contributor

---
+++ data/Tomlinson_2018/raw/Tomlinson_2018 extra R code.R
@@ -0,0 +1,27 @@
+read_csv("data/Tomlinson_2018/raw/Tomlinson Austraits locations.csv") %>%
+  select(SPECIES, `Seed location`) %>%
+  rename(Species = SPECIES) -> site_names
+
+read_csv("data/Tomlinson_2018/raw/week_5_data.csv") %>%
+  subset(Continent == "Australia") %>%
+  select(Individual,Species, Batch, 'Nutrient Treatment', 'Grass Treatment', STRL_m_per_g) -> week_05
+
+read_csv("data/Tomlinson_2018/raw/week_20_data.csv") %>%
+  subset(Continent == "Australia") %>%
+  mutate(photosynthetic_rate_per_area_saturated = A_nmol_CO2_per_g_s / SLA_mm2_per_mg,
+         PWUE_2 = photosynthetic_rate_per_area_saturated/gs_mmol_H2O_per_m2_s) %>%
+  full_join(week_05, by=c("Individual","Species", "Batch", "Nutrient Treatment", "Grass Treatment")) %>% 
+  full_join(site_names,by="Species") %>%
+  mutate(context = paste("Nutrient_treatment_",`Nutrient Treatment`,"_and_grass_treatment_",`Grass Treatment`,sep="")) %>%
+  write_csv("data/Tomlinson_2018/data.csv") %>%
+  select(context,`Nutrient Treatment`,`Grass Treatment`) %>%
+  group_by(context) %>%
+  mutate_at(vars(`Nutrient Treatment`,`Grass Treatment`),funs(replace(.,duplicated(.),NA))) %>%
+  subset(!is.na(`Nutrient Treatment`)) -> context_table
+
+context_table %>%
+  mutate(type = "treatment_favourable",
+         description = "high nutrient and grass competition") -> context_table
+
+
+read_csv("data/Tomlinson_2018/raw/sites.csv") -> sites
