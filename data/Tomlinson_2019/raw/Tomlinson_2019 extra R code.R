207802c63f000345e8e02e662a123f5afee8ae3c
Elizabeth Wenk <ehwenk@gmail.com>
2020-05-01 13:06:39 +1000

    Tomlinson_2019: lots of small changes from report review

--- data/Tomlinson_2019/raw/Tomlinson_2019 extra R code.R
+++ data/Tomlinson_2019/raw/Tomlinson_2019 extra R code.R
@@ -9,9 +9,9 @@ read_csv("data/Tomlinson_2018/raw/week_5_data.csv") %>%
 read_csv("data/Tomlinson_2018/raw/week_20_data.csv") %>%
   subset(Continent == "Australia") %>%
   mutate(photosynthetic_rate_per_area_saturated = A_nmol_CO2_per_g_s / SLA_mm2_per_mg,
-         PWUE_2 = photosynthetic_rate_per_area_saturated/gs_mmol_H2O_per_m2_s) %>%
+         WUE_intrinsic = photosynthetic_rate_per_area_saturated/gs_mmol_H2O_per_m2_s) %>%
   full_join(week_05, by=c("Individual","Species", "Batch", "Nutrient Treatment", "Grass Treatment")) %>% 
-  full_join(site_names,by="Species") %>%
+  left_join(site_names,by="Species") %>%
   mutate(context = paste("Nutrient_treatment_",`Nutrient Treatment`,"_and_grass_treatment_",`Grass Treatment`,sep="")) %>%
   write_csv("data/Tomlinson_2018/data.csv") %>%
   select(context,`Nutrient Treatment`,`Grass Treatment`) %>%
