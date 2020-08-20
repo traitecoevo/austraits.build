a885374813c60c504728eed3def2a57a1e602245
Elizabeth Wenk <ehwenk@gmail.com>
2020-08-20 14:01:11 +1000

    Everingham_2020: input new study

---
+++ data/Everingham_2020/raw/Everingham_2020 extra R code.R
@@ -0,0 +1,33 @@
+read_csv("data/Everingham_2020/raw/SE_Germination Data.csv") %>% 
+  select(-"Latitude (deg min sec S/N)",-"Longitude (deg min sec E/W)") -> germination
+
+read_csv("data/Everingham_2020/raw/SE_LMA data.csv") %>% 
+  select(-"Latitude (deg min sec S/N)",-"Longitude (deg min sec E/W)") -> LMA
+
+read_csv("data/Everingham_2020/raw/SE_Photosynthesis data.csv") %>% 
+  select(-"Latitude (deg min sec S/N)",-"Longitude (deg min sec E/W)") %>%
+  group_by(`Species`,`Modern/Historic`,`Date Collected`,`latitude (deg)`,`longitude (deg)`,`site`,`Plant ID`) %>%
+  summarise_all(.funs = mean) %>%
+  ungroup() -> photosynthesis 
+
+read_csv("data/Everingham_2020/raw/SE_Seed dimension data.csv") %>% 
+  select(-"Latitude (deg min sec S/N)",-"Longitude (deg min sec E/W)","Length transformed","Width transformed",
+         "Depth transformed","Sum","Variance") -> seed_size
+
+read_csv("data/Everingham_2020/raw/SE_Seed mass data.csv") %>% 
+  select(-"Latitude (deg min sec S/N)",-"Longitude (deg min sec E/W)") -> seed_mass
+
+read_csv("data/Everingham_2020/raw/SE_stem density data.csv") %>% 
+  select(-"Latitude (deg min sec S/N)",-"Longitude (deg min sec E/W)") -> stem_density
+
+seed_size %>%
+  bind_rows(seed_mass) %>%
+  bind_rows(LMA) %>%
+  bind_rows(stem_density) %>%
+  bind_rows(germination) %>%
+  bind_rows(photosynthesis) %>%
+  write_csv("data/Everingham_2020/data.csv") %>%
+  distinct(site, .keep_all = TRUE) %>%
+  select(`latitude (deg)`, `longitude (deg)`, `site`) -> site
+
+View(read_csv("data/Everingham_2020/data.csv", guess_max = 10000))
