c9415d9e536b641ead549cca1ce5ecca25c0d022
Elizabeth Wenk <ehwenk@gmail.com>
2020-12-18 11:18:32 +1100

    Schoenenberger_2020: initial AusTraits input
    AusTraits builds, haven't yet error checked, added date column, or matched taxa

---
+++ data/Schoenenberger_2020/raw/Schoenenberger_2020 extra R code.R
@@ -0,0 +1,31 @@
+read_csv("data/Schoenenberger_2020/raw/data_raw.csv") %>% 
+  rename(trait_name = Character) %>%
+  distinct(CharacterState,.keep_all = TRUE) %>% write_csv("data/Schoenenberger_2020/raw/traits_values1.csv")
+
+read_csv("data/Schoenenberger_2020/raw/data_raw.csv") %>%
+  subset(!is.na(CharacterState)) %>% 
+  mutate(trait_value = CharacterState) -> character_values
+
+read_csv("data/Schoenenberger_2020/raw/data_raw.csv") %>%
+  subset(!is.na(Value)) %>% 
+  mutate(trait_value = Value) %>%
+  mutate(trait_value = as.character(trait_value)) -> mean_values
+
+read_csv("data/Schoenenberger_2020/raw/data_raw.csv") %>%
+  subset(!is.na(Min)) %>% 
+  mutate(trait_value = Min) %>%
+  mutate(Character = paste0(Character,"_min")) %>%
+  mutate(trait_value = as.character(trait_value)) -> min_values
+
+read_csv("data/Schoenenberger_2020/raw/data_raw.csv") %>%
+  subset(!is.na(Max)) %>% 
+  mutate(trait_value = Max) %>%
+  mutate(Character = paste0(Character,"_max")) %>%
+  mutate(trait_value = as.character(trait_value)) -> max_values
+
+character_values %>%
+  bind_rows(mean_values) %>%
+  bind_rows(min_values) %>%
+  bind_rows(max_values) %>%
+  write_csv("data/Schoenenberger_2020/data.csv")
+  
