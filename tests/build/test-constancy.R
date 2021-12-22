f6020bfe34061ba42c022b7542be3d2f32e2a9ed
Daniel Falster <daniel.falster@unsw.edu.au>
2021-12-22 14:28:56 +1100

    Enable more testing of build, with examples
    - functions to test building a study and testing structure
    - placeholder for test examples

--- tests/build/test-constancy.R
+++ tests/build/test-constancy.R
@@ -36,8 +36,9 @@ build_comparison_set <- function(root.dir = rprojroot::find_root("remake.yml")) 
   austraits_raw[c("traits", "sites", "contexts", "methods", "excluded_data", "sources")]
 }
 
-test_that("constancy of product", {
 
+test_that("constancy of product", {
+  
   # some datasets to compare against
   expect_no_error(austraits_raw <- build_comparison_set(root.dir), info = "Building comparison set")
   
@@ -51,46 +52,5 @@ test_that("constancy of product", {
   for(v in names(austraits_raw)) {
     expect_equal(austraits_raw[[v]], austraits_raw_comparison[[v]], info = paste("comparing", v, "to ", file_comparison), ignore_attr = TRUE)
   }
- 
+  
 })
-
-
-test_that("structure of asset", {
-
-  root.dir = rprojroot::find_root("remake.yml")
-  
-  definitions <- yaml::read_yaml(file.path(root.dir, "config/definitions.yml"))
-  unit_conversions <- austraits.build:::make_unit_conversion_functions(file.path(root.dir, "config/unit_conversions.csv"))
-  taxon_list <- read_csv_char(file.path(root.dir, "config/taxon_list.csv"))
-
-  
-  expect_no_error({
-    Catford_2014_config <- austraits.build:::subset_config(file.path(root.dir, "data/Catford_2014/metadata.yml"), definitions, unit_conversions)
-    Catford_2014 <- austraits.build:::load_study(file.path(root.dir, "data/Catford_2014/data.csv"), Catford_2014_config)
-  }, info = "Building Catford_2014")
-  
-  expect_no_error({
-    Falster_2005_1_config <- austraits.build:::subset_config(file.path(root.dir, "data/Falster_2005_1/metadata.yml"), definitions, unit_conversions)
-    Falster_2005_1 <- austraits.build:::load_study(file.path(root.dir, "data/Falster_2005_1/data.csv"), Falster_2005_1_config)
-  }, info = "Building Falster_2005_1")
-  
-  expect_no_error({austraits1 <- austraits.build:::combine_austraits(Catford_2014, Falster_2005_1, definitions=definitions)}, 
-    info = "Combining sources")
-  
-  expect_no_error({austraits <- austraits.build:::update_taxonomy(austraits1, taxon_list)}, 
-    info = "Updating taxonomy")
-  
-  vars <- definitions$austraits$elements %>% names()
-  vars_tables <- vars[!(vars %in% c("definitions", "sources", "build_info"))]
-  
-  test_list_named(austraits, vars, info = "Main elements")
-
-  for(v in vars_tables) {
-    test_dataframe_named(austraits[[v]], definitions$austraits$elements[[v]]$elements %>% names(), info = paste("structure of ", v))
-  }
-
-  # contains allowed traits
-  expect_isin(austraits$traits$trait_name %>% unique(), definitions$traits$elements %>% names(), info = paste("traits ", v))
-
-})
-
