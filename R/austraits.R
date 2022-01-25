5923b0820613a6d950cf8ef9bd7b1f271f192702
Gary Truong <67947493+GaryTruong@users.noreply.github.com>
2022-01-25 14:00:42 +1100

    Add back in functions for checking suspected duplicates
    Added back in functions for checking and removing suspected duplicates with additional roxygen documentation. Functions were removed mistakenly in a previous commit

--- R/austraits.R
+++ R/austraits.R
@@ -1,3 +1,105 @@
+#' Check possible duplicates across studies
+#' 
+#' `label_suspected_duplicates` checks across studies to identify possible
+#' duplicates
+#'
+#' @param austraits_traits AusTraits traits data frame
+#' @param priority_sources default is NULL, a vector of studies to check against 
+#' for duplicates
+#'
+#' @importFrom rlang .data
+#' @return data frame containing potential duplicated data
+label_suspected_duplicates <- function(austraits_traits, priority_sources = NULL) {
+  
+  # copy traits and create a new variable with year of dataset_id
+  # we will preference studies with a lower value
+  if(is.null(priority_sources))
+    priority_sources <- 
+      c(
+        "Kew_2019_1", "Kew_2019_2", "Kew_2019_3", "Kew_2019_4", "Kew_2019_5", "Kew_2019_6",
+        "ANBG_2019", "GrassBase_2014", "CPBR_2002", "NTH_2014","RBGK_2014", 
+        "NHNSW_2016", "RBGSYD__2014_2", "RBGSYD_2014", "TMAG_2009", "WAH_1998", "WAH_2016",
+        "Brock_1993", "Barlow_1981", "Hyland_2003"    
+      )
+  
+  tmp <- austraits_traits %>% 
+    # Extract year from dataset_id, so that we can keep the older record
+    mutate(
+      priority_source = (.data$dataset_id %in% priority_sources),
+      year =  stringr::str_split(.data$dataset_id, "_") %>% 
+        lapply(function(i) i[2]) %>% unlist() %>% 
+        gsub("0000", "9999", .)
+    ) %>%
+    # sort to align suspected duplicates
+    dplyr::arrange(.data$trait_name, .data$taxon_name, .data$value, 
+                   dplyr::desc(.data$priority_source), .data$year) %>%
+    # detect duplicates based on combination of variables
+    dplyr::mutate(
+      to_check = paste(trait_name, taxon_name, value), 
+      duplicate = .data$to_check %>% duplicated()
+    ) %>% 
+    # remove temporary variables
+    dplyr::select(-.data$year, -.data$priority_source) %>%
+    # original sorting
+    dplyr::arrange(.data$observation_id, .data$trait_name, .data$value_type) %>% 
+    split(., .$duplicate)
+  
+  tmp[[2]] <- tmp[[2]] %>%
+    dplyr::mutate(
+      i = match(to_check, tmp[[1]]$to_check),
+      duplicate_dataset_id = tmp[[1]]$dataset_id[i],
+      duplicate_obs_id = tmp[[1]]$observation_id[i]
+    )
+  
+  tmp %>%  
+    dplyr::bind_rows() %>% 
+    dplyr::select(-.data$to_check, -.data$i) %>%
+    # original sorting
+    dplyr::arrange(.data$observation_id, .data$trait_name, .data$value_type)
+}
+
+#' Move suspected duplicates to excluded data
+#'
+#' `remove_suspected_duplicates` moves suspected duplicates from the 'traits' table 
+#' to excluded_data.
+#'
+#' @param austraits AusTraits data object
+#' @param priority_sources default = NULL. A vector of studies to check against
+#' for possible duplicates
+#' 
+#' @importFrom rlang .data
+#' @return AusTraits data object
+#' @export
+remove_suspected_duplicates <- function(austraits, 
+                                        priority_sources = NULL
+) {
+  tmp <- label_suspected_duplicates(austraits$traits, priority_sources)
+  
+  # update `traits` with unique values only
+  austraits$traits <- tmp %>% 
+    dplyr::filter(!.data$duplicate) %>% 
+    # remove temporary variables
+    dplyr::select(-tidyr::starts_with("duplicate")) %>%
+    # original sorting
+    dplyr::arrange(.data$observation_id, .data$trait_name, .data$value_type)
+  
+  # add suspected duplicates to bottom of excluded_data, noting 
+  # observation_id of the matching data in the error column
+  austraits$excluded_data <- austraits$excluded_data %>% 
+    dplyr::bind_rows(
+      tmp %>% 
+        dplyr::filter(.data$duplicate) %>% 
+        dplyr::mutate(error = sprintf("Duplicate of %s", .data$duplicate_obs_id)) %>%
+        # remove temporary variables
+        dplyr::select(-tidyr::starts_with("duplicate"))
+    ) %>%
+    # original sorting
+    dplyr::arrange(.data$observation_id, .data$trait_name, .data$value_type)
+  
+  austraits
+}
+
+
 ### function has been migrated to and modified in steps.R
 export_to_plain_text <- function(austraits, path) {
   dir.create(path, FALSE, TRUE)
