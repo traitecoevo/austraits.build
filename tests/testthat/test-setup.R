8dd8aa0c82578956821dac99d56e6c9e21b41639
Gary Truong <67947493+GaryTruong@users.noreply.github.com>
2022-05-30 15:20:40 +1000

    Update tests for test-setup.R (#580)
    * fix test errors for rcmdcheck
    
    * Update test-setup.R

--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -5,17 +5,17 @@ test_that("metadata_create_template is working",{
                                             path = file.path("data", "Test_2022"),
                                             skip_manual = TRUE))
   test_metadata <- read_metadata("data/Test_2022/metadata.yml")
-  
-  metadata_names <- c("source", "contributors", "dataset", "sites", "contexts", "traits", 
-                      "substitutions", "taxonomic_updates", "exclude_observations", 
+
+  metadata_names <- c("source", "contributors", "dataset", "sites", "contexts", "traits",
+                      "substitutions", "taxonomic_updates", "exclude_observations",
                       "questions")
   data_collector_names <- c("last_name", "given_name", "affiliation", "ORCID", "additional_role")
-  
-  ## Test metadata exists with correct names 
+
+  ## Test metadata exists with correct names
   expect_named(test_metadata)
   expect_equal(names(test_metadata), metadata_names)
   expect_equal(length(test_metadata$source$primary), 10)
-  
+
   ## Test contributors exist with the correct names
   expect_equal(length(test_metadata$contributors), 3)
   expect_equal(length(test_metadata$contributors$data_collectors), 5)
@@ -23,7 +23,7 @@ test_that("metadata_create_template is working",{
   expect_equal(length(test_metadata$contributors$assistants), 1)
   expect_equal(length(test_metadata$contributors$austraits_curators), 1)
   expect_equal(length(test_metadata$contributors$austraits_curators), 1)
-  
+
   expect_equal(length(test_metadata$dataset), 15)
 })
 
@@ -36,28 +36,30 @@ test_that("metadata_path_dataset_id is working",{
 test_that("metadata_read_dataset_id is working",{
   expect_silent(metadata_read_dataset_id("Test_2022"))
   expect_equal(class(metadata_read_dataset_id("Test_2022")), "list")
-})  
+})
 
 test_that("metadata_write_dataset_id is working",{
   metadata <- metadata_read_dataset_id("Test_2022")
-  
+
   unlink("data/Test_2022/metadata.yml")
   expect_false(file.exists("data/Test_2022/metadata.yml"))
-  
+
   expect_silent(metadata_write_dataset_id(metadata, "Test_2022"))
   expect_true(file.exists("data/Test_2022/metadata.yml"))
-  
+
   expect_silent(metadata_read_dataset_id("Test_2022"))
   expect_equal(class(metadata_read_dataset_id("Test_2022")), "list")
-})  
+})
 
 test_that("metadata_add_source_doi is working",{
   expect_invisible(metadata_add_source_doi(dataset_id = "Test_2022", doi = "https://doi.org/10.3389/fmars.2021.671145"))
-  expect_invisible(metadata_add_source_doi(dataset_id = "Test_2022", doi = "http://doi.org/10.3389/fmars.2021.671145"))
+  expect_equal(read_metadata("data/Test_2022/metadata.yml")$source$primary$year, "2021")
+  expect_invisible(metadata_add_source_doi(dataset_id = "Test_2022", doi = "http://doi.org/10.1111/j.0022-0477.2005.00992.x"))
+  expect_equal(read_metadata("data/Test_2022/metadata.yml")$source$primary$year, "2005")
   expect_invisible(metadata_add_source_doi(dataset_id = "Test_2022", doi = "doi.org/10.3389/fmars.2021.671145"))
+  expect_equal(read_metadata("data/Test_2022/metadata.yml")$source$primary$year, "2021")
   expect_invisible(metadata_add_source_doi(dataset_id = "Test_2022", doi = "10.3389/fmars.2021.671145"))
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$source$primary$journal, "Frontiers in Marine Science")
-  
   expect_silent(metadata_add_source_doi(dataset_id = "Test_2022", doi = "10.1111/j.0022-0477.2005.00992.x"))
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$source$primary$journal, "Journal of Ecology")
 })
@@ -67,7 +69,7 @@ test_that("metadata_check_custom_R_code is working",{
   expect_equal(class(metadata_check_custom_R_code("Test_2022")), c("spec_tbl_df", "tbl_df", "tbl", "data.frame"))
   expect_equal(ncol(metadata_check_custom_R_code("Test_2022")), 13)
   expect_equal(nrow(metadata_check_custom_R_code("Test_2022")), 45)
-  
+
   expect_visible(metadata_check_custom_R_code("Test_2022"))
 })
 
@@ -81,7 +83,7 @@ test_that("metadata_add_source_bibtex is working",{
 })
 
 test_that("metadata_add_substitution is working",{
-  expect_silent(suppressMessages(metadata_add_substitution("Test_2022", "specific_leaf_area", 
+  expect_silent(suppressMessages(metadata_add_substitution("Test_2022", "specific_leaf_area",
                                                            "leaf_area", "specific_leaf_area")))
   expect_equal(length(read_metadata("data/Test_2022/metadata.yml")$substitutions[[1]]), 3)
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$substitutions[[1]]$trait_name, "specific_leaf_area")
@@ -101,7 +103,7 @@ test_that("metadata_exclude_observations is working",{
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$exclude_observations[[1]]$variable, "stem")
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$exclude_observations[[1]]$find, "branch")
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$exclude_observations[[1]]$reason, "test")
-})  
+})
 
 test_that("metadata_update_taxonomic_change is working",{
   expect_error(metadata_update_taxonomic_change("Test_2022", "grass", "bark", "soil"))
@@ -109,7 +111,7 @@ test_that("metadata_update_taxonomic_change is working",{
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$taxonomic_updates[[1]]$find, "flower")
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$taxonomic_updates[[1]]$replace, "bark")
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$taxonomic_updates[[1]]$reason, "soil")
-})  
+})
 
 test_that("metadata_remove_taxonomic_change is working",{
   expect_invisible(metadata_remove_taxonomic_change("Test_2022", "flower"))
