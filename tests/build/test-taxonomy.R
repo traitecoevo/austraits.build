a79640b7d71676b29ce18184bf528ac4e692aea4
Daniel Falster <daniel.falster@unsw.edu.au>
2022-08-08 22:07:27 +1000

    fixes

--- tests/build/test-taxonomy.R
+++ tests/build/test-taxonomy.R
@@ -7,20 +7,10 @@ suppressWarnings({
   library(kableExtra)
 })
 
-test_that("test load_taxonomic_resources is working",{
-  suppressWarnings(rm(taxonomic_resources))
-  expect_no_error(x <- load_taxonomic_resources())
-  expect_equal(taxonomic_resources, x)
-  expect_named(x, c("APC", "APNI"))
-  expect_equal(length(x), 2)
-  expect_type(x$APC, "list")
-  expect_type(x$APNI, "list")
-})
-
-test_that("test build_setup_pipeline is working",{
-
-  unlink("config", FALSE)
-  unlink("data", FALSE)
+test_that("test taxonomic resources are working",{
+  
+  unlink("config", TRUE)
+  unlink("data", TRUE)
   expect_true(file.copy("../testthat/config", ".", recursive = TRUE, overwrite = TRUE))
   expect_true(file.copy("../testthat/data", ".", recursive = TRUE, overwrite = TRUE))
   expect_true(file.copy("data/Test_2022/test-metadata.yml", "data/Test_2022/metadata.yml", overwrite = TRUE))
@@ -28,6 +18,15 @@ test_that("test build_setup_pipeline is working",{
   expect_no_error(zip::unzip("../testthat/testgit.zip"))
   unlink("config/taxon_list.csv")
   expect_false(file.exists("config/taxon_list.csv"))
+  
+  suppressWarnings(rm(taxonomic_resources))
+  expect_no_error(x <- load_taxonomic_resources())
+  expect_equal(taxonomic_resources, x)
+  expect_named(x, c("APC", "APNI"))
+  expect_equal(length(x), 2)
+  expect_type(x$APC, "list")
+  expect_type(x$APNI, "list")
+
   expect_silent(build_setup_pipeline())
   expect_true(file.exists("config/taxon_list.csv"))
   expect_no_error(austraits_raw <- remake::make("austraits_raw"))
