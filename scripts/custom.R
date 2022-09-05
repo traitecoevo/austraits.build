a2a2a21c43050893841caf00c5e7265618376536
ehwenk <ehwenk@gmail.com>
2022-09-05 10:29:22 +1000

    aligning plant growth forms

--- scripts/custom.R
+++ scripts/custom.R
@@ -274,6 +274,8 @@ move_values_to_new_trait <- function(data, original_trait, new_trait, original_v
             data
        }
        
+  data = data %>% mutate_all(na_if,"")
+
   return(data)
 }
 
