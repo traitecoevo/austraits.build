8e6f3070142ecc566f73daa756dfb321f47137d5
Daniel Falster <daniel.falster@unsw.edu.au>
2022-11-17 13:00:07 +1100

    Modify taxonomy tests

--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -164,9 +164,15 @@ test_that("test build_setup_pipeline is working",{
   expect_silent(taxa1 <- read_csv_char("config/taxon_list.csv"))
   vars <- c("cleaned_name", "source", "taxonIDClean", "taxonomicStatusClean", "alternativeTaxonomicStatusClean", "acceptedNameUsageID", "taxon_name", "scientificNameAuthorship", "taxonRank", "taxonomicStatus", "family", "taxonDistribution", "ccAttributionIRI")
   expect_named(taxa1, vars)
-  expect_length(taxa1, 13)
+  expect_length(taxa1, 15)
   expect_true(nrow(taxa1) == 0)
   
+  expect_true(file.copy("config/taxon_list-orig.csv", "config/taxon_list.csv", TRUE))
+  expect_silent(taxa2 <- read_csv_char("config/taxon_list.csv"))
+  expect_named(taxa2, vars)
+  expect_length(taxa2, 13)
+  expect_true(nrow(taxa2) == 5)
+ 
   expect_no_error(austraits <- remake::make("austraits"))
   
   expect_null(austraits_raw$build_info$version)
