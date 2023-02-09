fe210b0e9a0d802a0fcba1f3edf74e2f6b107dd9
ehwenk <ehwenk@gmail.com>
2023-02-10 10:01:07 +1100

    continued work toward dplyr 1.1.0 compliance
    The only remaining code that works differently than before is in custom.R, the function `move_values_to_new_trait`. The final line to convert blank cells to na's no longer triggers.  Instead this line now has to be added to the custom R code of each dataset that uses this function. I've fixed all datasets where lots of blanks were created, but there are a few datasets where I'm just leaving the blank cells (appear in excluded_data)

--- scripts/custom.R
+++ scripts/custom.R
@@ -281,8 +281,7 @@ move_values_to_new_trait <- function(data, original_trait, new_trait, original_v
             data
        }
          
-  #data = data %>% mutate_all(na_if,"")
-  data %>% mutate(across(where(is.character), ~na_if(.x, "")))
+    data = data %>% mutate(across(everything(), ~na_if(.x, "")))
   
   return(data)
 
