01684d6eb5ce0e04598a170e9b21cd4b1d05c9ac
Elizabeth Wenk <ehwenk@gmail.com>
2023-02-09 20:42:26 +1100

    comply with dplyr 1.1.0
    Various changes to comply with dplyr 1.1.0, including changes to fix things that broke and those that will in the future
    
    * changed as ~na_if(.,0) to ~na_if(.x,0)
    * moved ~na_if statements that were being piped into summarise()
    * replaced all use of "vars" with "c"
    * replaced all use of "mutate_at" with "mutate(across())"
    
    Of all studies, only Kooyman_2011 had any changes - about 50 rows added, but not sure why

--- scripts/custom.R
+++ scripts/custom.R
@@ -282,7 +282,7 @@ move_values_to_new_trait <- function(data, original_trait, new_trait, original_v
        }
          
   #data = data %>% mutate_all(na_if,"")
-  data %>% mutate(across(where(is.character), ~na_if(., "")))
+  data %>% mutate(across(where(is.character), ~na_if(.x, "")))
   
   return(data)
 
