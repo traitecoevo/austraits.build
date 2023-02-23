9402b51b540d95a3cc11207fbc52008ab0a1d0e4
ehwenk <ehwenk@gmail.com>
2023-02-24 10:26:59 +1100

    Updates metadata, data following response to report
    * Added DBH (population median, min, max) as a context property
    * Added an additional data contributor
    
    ... Also fussed with reports.Rmd, but not getting line 373 to work. Checked all the snippets of code individually and they give the correct output, but not once I do the paste, ad writeLines. So they are again in quotes.

--- data/Prior_2022/raw/Prior_2022_extra_R_code.R
+++ data/Prior_2022/raw/Prior_2022_extra_R_code.R
@@ -1,5 +1,5 @@
 read_csv("data/Prior_2022/raw/data_tmp.csv") %>%
-  select(taxon_name, Alive, Scorch, Epicormic, Basal, Subplot, Location, `Char.ht`, Intensity) %>%
+  select(taxon_name, Alive, Scorch, Epicormic, Basal, Subplot, Location, `Char.ht`, Intensity, DBH) %>%
   mutate(replicates = 1) %>%
   group_by(taxon_name, Subplot, Location, Intensity) %>%
   summarise(
@@ -8,7 +8,10 @@ read_csv("data/Prior_2022/raw/data_tmp.csv") %>%
     basal_consensus = mean(Basal),
     `Char.ht` = mean(`Char.ht`, na.rm = TRUE),
     Scorch = mean(Scorch, na.rm = TRUE) %>% round(digits = 0),
-    replicates = sum(replicates)
+    replicates = sum(replicates),
+    DBH_median = median(DBH, na.rm = TRUE),
+    DBH_min = min(DBH, na.rm = TRUE),
+    DBH_max = max(DBH, na.rm = TRUE)
     ) %>%
   ungroup() %>%
   mutate(
@@ -19,7 +22,10 @@ read_csv("data/Prior_2022/raw/data_tmp.csv") %>%
     fire_response = ifelse(resprout_mean > 0.7, "resprouts", NA),
     fire_response = ifelse(resprout_mean < 0.3, "fire_killed", fire_response),
     fire_response = ifelse(resprout_mean < 0.7 & resprout_mean > 0.3, "partial_resprouting", fire_response),
-    across(c(`Char.ht`, Scorch), ~ na_if(.x, "NaN")),
-    Scorch = ifelse(is.na(Scorch), "unknown", Scorch)
+    across(c(`Char.ht`, Scorch, DBH_median), ~ na_if(.x, "NaN")),
+    across(c(`Char.ht`, Scorch, DBH_min), ~ na_if(.x, "Inf")),
+    across(c(`Char.ht`, Scorch, DBH_max), ~ na_if(.x, "-Inf")),
+    Scorch = ifelse(is.na(Scorch), "unknown", Scorch),
+    DBH = ifelse((!is.na(DBH_median) & round(DBH_median,0) != round(DBH_min,0)),paste0(DBH_median," (", DBH_min," - ",DBH_max,")"),DBH_median)
   ) %>%
   write_csv("data/Prior_2022/data.csv")
