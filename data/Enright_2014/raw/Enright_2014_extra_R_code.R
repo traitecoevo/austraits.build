44711d1f7f5d1c2c02855add9d06e8801efb474a
yangsophieee <sophie.yang917@gmail.com>
2023-05-18 13:17:14 +1000

    Remove some columns from data

--- data/Enright_2014/raw/Enright_2014_extra_R_code.R
+++ data/Enright_2014/raw/Enright_2014_extra_R_code.R
@@ -6,12 +6,14 @@ data2 <- read_csv("data/Enright_2014/raw/PFT_datasetJEcol_2_ResproutersSeedAdult
 plot_data <- read_csv("data/Enright_2014/raw/PFT_PlotScaleData_JEcol.csv")
 
 data1 %>% full_join(data2) -> data
-data %>% left_join(plot_data) -> data
+#data %>% left_join(plot_data) -> data
 
 data %>% 
   mutate(
     taxon_name = paste(genus, species)
   ) %>% 
-  relocate(taxon_name, .before = genus) -> data
+  relocate(taxon_name, .before = genus) %>% 
+  select(-Chng, -plotID, -subplot, -plot.sub, -KangarooScat, -RegenType) %>% 
+  distinct(.keep_all = TRUE) -> data
 
 data %>% write_csv("data/Enright_2014/data.csv")
