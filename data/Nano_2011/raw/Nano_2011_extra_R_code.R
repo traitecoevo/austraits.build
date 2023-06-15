2c444f58d7a8c2db9e5ea75e37987b923b715687
yangsophieee <sophie.yang917@gmail.com>
2023-06-15 12:20:48 +1000

    Move Crowley data to its own folder

--- data/Nano_2011/raw/Nano_2011_extra_R_code.R
+++ data/Nano_2011/raw/Nano_2011_extra_R_code.R
@@ -6,6 +6,8 @@ original_data <- read_csv("data/Nano_2011/raw/original_data.csv")
 more_resprouting_data <- 
   read_xlsx("data/Nano_2011/raw/ecological_attributes.xlsx", sheet = "Resprouting")
 
+#more_resprouting_data %>% write_csv("data/Nano_2011/raw/Crowley_data.csv")
+
 detailed_data <- 
   read_xlsx("data/Nano_2011/raw/ecological_attributes.xlsx", sheet = "ecological_attributes") %>% 
   select(`Taxon Name`, `Eco Source`, `Fire Response Adult`, `Resprout Type`, `Seed Bank`) %>% 
@@ -17,10 +19,9 @@ stem_buds_data <-
 
 original_data %>% 
   full_join(detailed_data, by = c("Taxon" = "Taxon Name")) %>% 
-  full_join(more_resprouting_data, by = c("Taxon" = "Nameinfra")) %>% 
-  full_join(stem_buds_data, by = c("Taxon" = "species")) %>% 
-  select(-Family) -> joined
+  full_join(stem_buds_data, by = c("Taxon" = "species")) -> joined
 
 #joined %>% filter(!is.na(`Vegetative persistence`), !is.na(Fire_Response_Adult)) %>% View()
 
-joined %>% write_csv("data/Nano_2011/raw/joined_data.csv")
+#joined %>% write_csv("data/Nano_2011/raw/joined_data.csv")
+joined %>% write_csv("data/Nano_2011/data.csv")
