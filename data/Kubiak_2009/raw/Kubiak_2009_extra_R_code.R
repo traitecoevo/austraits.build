ad373c22afdda11e9a39b441f97c5d6da19d7a5b
Sophie Yang <78716301+yangsophieee@users.noreply.github.com>
2023-06-16 16:59:58 +1000

    Update with extra data missed when transcribing

--- data/Kubiak_2009/raw/Kubiak_2009_extra_R_code.R
+++ data/Kubiak_2009/raw/Kubiak_2009_extra_R_code.R
@@ -6,8 +6,9 @@ wide_data <- read_csv("data/Kubiak_2009/raw/wide_data.csv")
 wide_data %>% 
   pivot_longer(
     c(fire_response_LCR, fire_response_NL, seedlings_first_observed, first_flowering_resp,
-      first_flowering_seed, first_fruiting_resp, first_fruiting_seed, primary_juvenile_period,
-      secondary_juvenile_period, peak_flowering, peak_fruiting),
+      first_flowering_seed, first_flowering_unknown, first_fruiting_resp, first_fruiting_seed, 
+      first_fruiting_unknown, primary_juvenile_period, secondary_juvenile_period, 
+      peak_flowering, peak_fruiting),
     names_to = "trait_name",
     values_to = "value"
   ) -> long_data
