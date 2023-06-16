a0d9fc923bf151b06c25c9ba902a1fa4b8ecb5e6
ehwenk <ehwenk@gmail.com>
2023-06-16 17:09:21 +1000

    Create build_traits_yml_from_APD.R

---
+++ scripts/build_traits_yml_from_APD.R
@@ -0,0 +1,71 @@
+library(purrr)
+
+# XXX need to source APD_traits.csv and APD_categorical_values.csv from APD/data
+# XXX need to change triple quotes (''') to single quotes ('). During read/write, '{count}/{count}' becomes '''{count}/{count}'''
+# XXX flowering_time, fruiting_time and recruitment_time are categorical traits but without any set categorical trait values. 
+# XXX       Therefore an empty value for allowed_values_levels is created that breaks AusTraits
+
+APD <- 
+  read_csv("export/data/APD_traits.csv", show_col_types = FALSE) %>%
+  mutate(
+    description = ifelse(is.na(description), description_encoded, paste0(description_encoded, ";", description)),
+    units = ifelse(str_starts(units, fixed("{")), sprintf("'%s'", units),units),
+    Entity_URI = paste0("https://w3id.org/APD/traits/", identifier),
+    allowed_values_levels = ifelse(type_x == "categorical", "add_in", NA),
+  ) %>%
+  select(trait,
+         label,
+         description,
+         comments,
+         type = type_x,
+         units,
+         allowed_values_min = min,
+         allowed_values_max = max,
+         allowed_values_levels,
+         entity_URI = Entity_URI
+  ) %>%
+  arrange(entity_URI)
+
+value_levels <- read_csv("export/data/APD_categorical_values.csv", show_col_types = FALSE) %>%
+  select(trait_cat = trait_name,
+         label,
+         description
+  )
+
+traits <- get_schema("export/traits2.yml", I("traits"))
+
+vars <- c("label", "description", "comments", "type", "units", "allowed_values_min", "allowed_values_max", "allowed_values_levels", "entity_URI")
+
+traits$elements <-  
+  APD %>%
+  split(APD$entity_URI) %>% # turn into list, 1 element for each row
+  # reprocess rows into a list
+  map(~ .x %>% 
+        # variables in specified order
+        select(all_of(vars)) %>%
+        # remove NAs
+        select(where(~ !is.na(.x))) %>%
+        # as a list
+        as.list()
+  )
+
+# rename with trait name
+all.equal(names(traits$elements), APD$entity_URI)
+names(traits$elements) <- APD$trait
+
+
+# for add categorical traits, add allowable values
+for(trait in names(traits$elements)) {
+  if(traits$elements[[trait]][["type"]] == "categorical") {      
+    traits$elements[[trait]][["allowed_values_levels"]] <- 
+      value_levels %>% 
+      filter(trait_cat == trait) %>% 
+      select(label, description) %>%
+      arrange(label) %>%
+      pivot_wider(names_from = label, values_from = description) %>%
+      
+      as.list()
+  }
+}
+
+yaml::write_yaml(list(traits = traits), "config/traits.yml")
