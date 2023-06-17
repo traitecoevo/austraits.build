b68057899ee4baf750dc5e17071c0fa22b188c91
Sophie Yang <78716301+yangsophieee@users.noreply.github.com>
2023-06-17 19:33:15 +1000

    Add code to extract values out of columns

--- data/Kubiak_2009/raw/Kubiak_2009_extra_R_code.R
+++ data/Kubiak_2009/raw/Kubiak_2009_extra_R_code.R
@@ -4,6 +4,81 @@ library(tidyverse)
 wide_data <- read_csv("data/Kubiak_2009/raw/wide_data.csv")
 
 wide_data %>% 
+  mutate(
+    seedlings_first_observed = str_extract(seedlings_first_observed, "[0-9\\-]+(?=w)"),
+    first_flowering_resp = if_else(
+      str_detect(first_flowering_resp, "y"),
+      str_extract(first_flowering_resp, "[0-9\\-]+(?=y)"),
+      str_extract(first_flowering_resp, "[0-9\\-]+(?=w)")    
+    ),
+    first_flowering_seed = if_else(
+      str_detect(first_flowering_seed, "y"),
+      str_extract(first_flowering_seed, "[0-9\\-]+(?=y)"),
+      str_extract(first_flowering_seed, "[0-9\\-]+(?=w)")    
+    ),
+    first_flowering_unknown = if_else(
+      str_detect(first_flowering_unknown, "y"),
+      str_extract(first_flowering_unknown, "[0-9\\-]+(?=y)"),
+      str_extract(first_flowering_unknown, "[0-9\\-]+(?=w)")    
+    ),
+    first_fruiting_resp = if_else(
+      str_detect(first_fruiting_resp, "y"),
+      str_extract(first_fruiting_resp, "[0-9\\-]+(?=y)"),
+      str_extract(first_fruiting_resp, "[0-9\\-]+(?=w)")    
+    ),
+    first_fruiting_seed = if_else(
+      str_detect(first_fruiting_seed, "y"),
+      str_extract(first_fruiting_seed, "[0-9\\-]+(?=y)"),
+      str_extract(first_fruiting_seed, "[0-9\\-]+(?=w)")    
+    ),
+    first_fruiting_unknown = if_else(
+      str_detect(first_fruiting_unknown, "y"),
+      str_extract(first_fruiting_unknown, "[0-9\\-]+(?=y)"),
+      str_extract(first_fruiting_unknown, "[0-9\\-]+(?=w)")    
+    ),
+    primary_juvenile_period = if_else(
+      str_detect(primary_juvenile_period, "y"),
+      str_extract(primary_juvenile_period, "[0-9\\-]+(?=y)"),
+      str_extract(primary_juvenile_period, "[0-9\\-]+(?=w)")    
+    ),
+    secondary_juvenile_period = case_when(
+      str_detect(secondary_juvenile_period, "\\+") ~ str_extract(secondary_juvenile_period, "[0-9\\-]+(?=\\+)"),
+      str_detect(secondary_juvenile_period, "y") ~ str_extract(secondary_juvenile_period, "[0-9\\-]+(?=y)"),
+      TRUE ~ str_extract(secondary_juvenile_period, "[0-9\\-]+(?=w)")
+    ),
+    peak_flowering_resp = if_else(
+      str_detect(peak_flowering_resp, "\\+"),
+      str_extract(peak_flowering_resp, "[0-9\\-]+(?=\\+)"),
+      str_extract(peak_flowering_resp, "[0-9\\-]+(?=y)")    
+    ),
+    peak_flowering_seed = if_else(
+      str_detect(peak_flowering_seed, "\\+"),
+      str_extract(peak_flowering_seed, "[0-9\\-]+(?=\\+)"),
+      str_extract(peak_flowering_seed, "[0-9\\-]+(?=y)")    
+    ),
+    peak_flowering_unknown = if_else(
+      str_detect(peak_flowering_unknown, "\\+"),
+      str_extract(peak_flowering_unknown, "[0-9\\-]+(?=\\+)"),
+      str_extract(peak_flowering_unknown, "[0-9\\-]+(?=y)")    
+    ),
+    peak_fruiting_resp = if_else(
+      str_detect(peak_fruiting_resp, "\\+"),
+      str_extract(peak_fruiting_resp, "[0-9\\-]+(?=\\+)"),
+      str_extract(peak_fruiting_resp, "[0-9\\-]+(?=y)")    
+    ),
+    peak_fruiting_seed = if_else(
+      str_detect(peak_fruiting_seed, "\\+"),
+      str_extract(peak_fruiting_seed, "[0-9\\-]+(?=\\+)"),
+      str_extract(peak_fruiting_seed, "[0-9\\-]+(?=y)")    
+    ),
+    peak_fruiting_unknown = if_else(
+      str_detect(peak_fruiting_unknown, "\\+"),
+      str_extract(peak_fruiting_unknown, "[0-9\\-]+(?=\\+)"),
+      str_extract(peak_fruiting_unknown, "[0-9\\-]+(?=y)")    
+    )
+  ) -> wide_data
+
+wide_data %>% 
   pivot_longer(
     c(fire_response_LCR, fire_response_NL, seedlings_first_observed, first_flowering_resp,
       first_flowering_seed, first_flowering_unknown, first_fruiting_resp, first_fruiting_seed, 
@@ -12,6 +87,7 @@ wide_data %>%
       peak_fruiting_seed, peak_fruiting_unknown),
     names_to = "trait_name",
     values_to = "value"
-  ) -> long_data
+  ) %>% 
+  drop_na(value) -> long_data
 
 long_data %>% write_csv("data/Kubiak_2009/data.csv")
