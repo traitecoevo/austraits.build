56e9c2fd470647be45539f50ff4a57b6b4319738
yangsophieee <sophie.yang917@gmail.com>
2023-06-22 11:34:31 +1000

    Duplicate primary_juvenile_period column to map to another trait

--- data/Kubiak_2009/raw/Kubiak_2009_extra_R_code.R
+++ data/Kubiak_2009/raw/Kubiak_2009_extra_R_code.R
@@ -44,6 +44,7 @@ wide_data %>%
       str_extract(primary_juvenile_period, "[0-9][0-9\\-\\.]*(?=y)"),
       str_extract(primary_juvenile_period, "[0-9\\-]+(?=w)")    
     ),
+    fire_time_from_fire_to_flowering = primary_juvenile_period,
     secondary_juvenile_period = case_when(
       str_detect(secondary_juvenile_period, "\\+") ~ str_extract(secondary_juvenile_period, "[0-9\\-]+(?=\\+)"),
       str_detect(secondary_juvenile_period, "y") ~ str_extract(secondary_juvenile_period, "[0-9\\-][0-9\\-\\.]*(?=y)"),
@@ -100,10 +101,10 @@ wide_data %>%
   pivot_longer(
     c(fire_response_LCR, fire_response_NL, seedlings_first_observed, first_flowering_resp,
       first_flowering_seed, first_flowering_unknown, first_fruiting_resp, first_fruiting_seed, 
-      first_fruiting_unknown, primary_juvenile_period, secondary_juvenile_period, 
-      peak_flowering_resp, peak_flowering_seed, peak_flowering_unknown, peak_fruiting_resp, 
-      peak_fruiting_seed, peak_fruiting_unknown, large_prop_flowering_resp, large_prop_flowering_seed,
-      large_prop_flowering_unknown, dispersal_unit),
+      first_fruiting_unknown, primary_juvenile_period, fire_time_from_fire_to_flowering,
+      secondary_juvenile_period, peak_flowering_resp, peak_flowering_seed, peak_flowering_unknown,
+      peak_fruiting_resp, peak_fruiting_seed, peak_fruiting_unknown, large_prop_flowering_resp, 
+      large_prop_flowering_seed, large_prop_flowering_unknown, dispersal_unit),
     names_to = "trait_name",
     values_to = "value"
   ) %>% 
