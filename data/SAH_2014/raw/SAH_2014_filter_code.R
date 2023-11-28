8331f19ca82b1e66121f7bddb08c6ede02be1851
Elizabeth Wenk <ehwenk@gmail.com>
2023-11-28 11:42:54 +1100

    filter SAH_2014
    filter SAH_2014 to only retain trait values that aren't also in SAH_2022, SAH_2023. As part of this, SAH_2014 is now in long-format.

---
+++ data/SAH_2014/raw/SAH_2014_filter_code.R
@@ -0,0 +1,36 @@
+austraits$traits %>% 
+  filter(dataset_id %in% c("SAH_2014")) %>%
+  select(taxon_name, trait_name, value_type, value, unit, observation_id, original_name) %>% 
+  filter(!(trait_name == "plant_height" & value_type == "minimum")) %>%
+  distinct() %>%
+  group_by(taxon_name, trait_name, value_type) %>%
+  mutate(value = paste(value, collapse = " ")) %>%
+  ungroup() -> SAH_old
+
+
+tmp <- austraits %>%
+  austraits::extract_dataset(c("SAH_2022", "SAH_2023")) %>%
+  austraits::join_contexts()
+  
+tmp$traits %>%
+  select(taxon_name, trait_name, value_type, value, `entity measured`) %>%
+  filter(!`entity measured` %in% c("cauline leaf", "leaflet", "pinnae")) %>% #100+ more unmatched if you remove this
+  select(-`entity measured`) %>%
+  group_by(taxon_name, trait_name, value_type) %>%
+  mutate(value = paste(value, collapse = " ")) %>%
+  ungroup() %>%
+  rename(value_new = value) -> SAH_new
+
+SAH_old %>% 
+  left_join(SAH_new) %>%
+  filter(is.na(value_new)) %>%
+  group_by(trait_name) %>% mutate(n_taxa = n()) %>% ungroup() %>% distinct(trait_name, n_taxa) %>% View()
+
+SAH_old %>%
+  distinct(taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>%
+  distinct() %>%
+  left_join(SAH_new) %>%
+  filter(is.na(value_new)) %>%
+  select(-value_new) -> SAH_2014_retain
+
+SAH_2014_retain %>% write_csv("data/SAH_2014/data.csv")
