33830d6465cc724a2a7ac6be66f03133087f328a
ehwenk <ehwenk@gmail.com>
2023-11-30 21:53:15 +1100

    WAH_1998, WAH_2016
    easy, because virtually no numeric traits, almost all trait values that were only in WAH_1998 were flowering times

--- data/WAH_1998/raw/WAH_filter_duplicate_values.R
+++ data/WAH_1998/raw/WAH_filter_duplicate_values.R
@@ -1,6 +1,6 @@
-ABRS_old <- austraits$traits %>% 
-  filter(dataset_id %in% c("ABRS_1981")) %>%
-  select(taxon_name, trait_name, value_type, value, unit, original_name, observation_id) %>% 
+WAH_old <- austraits$traits %>% 
+  filter(dataset_id %in% c("WAH_1998", "WAH_2016")) %>%
+  select(dataset_id, taxon_name, trait_name, value_type, value, unit, original_name, observation_id) %>% 
   distinct(taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>%
   filter(!(trait_name == "plant_height" & value_type == "minimum")) %>%
   distinct() %>%
@@ -9,10 +9,10 @@ ABRS_old <- austraits$traits %>%
   ungroup()
 
 tmp <- austraits %>%
-  austraits::extract_dataset(c("ABRS_2022", "ABRS_2023")) %>%
+  austraits::extract_dataset(c("WAH_2022_1", "WAH_2023_1", "WAH_2022_2", "WAH_2023_2")) %>%
   austraits::join_contexts()
 
-ABRS_new <- tmp$traits %>%
+WAH_new <- tmp$traits %>%
   select(taxon_name, trait_name, value_type, value) %>%
   distinct() %>%
   group_by(taxon_name, trait_name, value_type) %>%
@@ -20,37 +20,31 @@ ABRS_new <- tmp$traits %>%
   ungroup() %>%
   rename(value_new = value)
 
-counts_diff <- ABRS_old %>% 
+counts_diff <- WAH_old %>% 
   distinct(taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>% 
-  left_join(ABRS_new) %>%
+  left_join(WAH_new) %>%
   filter(is.na(value_new)) %>%
   filter(!trait_name %in% c("woodiness", "woodiness_detailed", "life_history", "plant_growth_form")) %>%
   group_by(trait_name) %>% mutate(n_taxa = n()) %>% ungroup() %>% distinct(trait_name, n_taxa) %>%
   arrange(n_taxa)
 
-retain_1 <- ABRS_old %>%
-  distinct(taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>%
-  left_join(ABRS_new) %>%
+retain_1 <- WAH_old %>%
+  distinct(dataset_id, taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>%
+  left_join(WAH_new) %>%
   filter(is.na(value_new)) %>%
   filter(!trait_name %in% c("woodiness", "woodiness_detailed", "life_history", "plant_growth_form"))
 
-retain_missing_taxa <- retain_1 %>% filter(!taxon_name %in% ABRS_new$taxon_name)
+retain_missing_taxa <- retain_1 %>% filter(!taxon_name %in% WAH_new$taxon_name)
 
 retain_2 <- retain_1 %>% anti_join(retain_missing_taxa)
 
-retain_trustworthy_traits <-
-  retain_2 %>%
-  filter(trait_name %in% c("leaf_phenology", "flowering_time", "leaf_compoundness", "dispersal_appendage", "plant_growth_substrate", "seed_shape"))
-      
-retain_seed_traits <-
-  retain_2 %>%
-  filter(trait_name %in% c("seed_length", "seed_width", "seed_height"))
+excluded <- retain_2 %>% filter(trait_name == "plant_height")
 
-excluded <- retain_2 %>% anti_join(retain_trustworthy_traits) %>% anti_join(retain_seed_traits)
-
-ABRS_retain <-
+WAH_retain <-
   retain_missing_taxa %>%
-  bind_rows(retain_trustworthy_traits) %>%
-  bind_rows(retain_seed_traits)
+  bind_rows(retain_2 %>% filter(trait_name != "plant_height")) %>%
+  select(-value_new)
 
-ABRS_retain %>% select(-value_new) %>% write_csv("data/ABRS_1981/data.csv")
+WAH_retain %>% filter(dataset_id == "WAH_1998") %>% write_csv("data/WAH_1998/data.csv")
+
+WAH_retain %>% filter(dataset_id == "WAH_2016") %>% write_csv("data/WAH_2016/data.csv")  
