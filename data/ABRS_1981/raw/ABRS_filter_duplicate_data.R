0841957822c0a904e529bc8fd5b14005ef312cf9
ehwenk <ehwenk@gmail.com>
2023-11-30 21:23:24 +1100

    ABRS_1981 - filter duplicates
    - have removed all leaf width, leaf lengths, plant heights EXCEPT if the taxa are completely missing from ABRS_2022, 2023. There are about 1000 values to manually check - well over 50% were wrong plant part or wrong number, so they are simply saved in the raw data folder

---
+++ data/ABRS_1981/raw/ABRS_filter_duplicate_data.R
@@ -0,0 +1,115 @@
+ABRS_old <- austraits$traits %>% 
+  filter(dataset_id %in% c("ABRS_1981")) %>%
+  select(taxon_name, trait_name, value_type, value, unit, original_name, observation_id) %>% 
+  distinct(taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>%
+  filter(!(trait_name == "plant_height" & value_type == "minimum")) %>%
+  distinct() %>%
+  group_by(taxon_name, trait_name, value_type) %>%
+  mutate(value = paste(value, collapse = " ")) %>% distinct() %>%
+  ungroup()
+
+tmp <- austraits %>%
+  austraits::extract_dataset(c("ABRS_2022", "ABRS_2023")) %>%
+  austraits::join_contexts()
+
+ABRS_new <- tmp$traits %>%
+  select(taxon_name, trait_name, value_type, value) %>%
+  distinct() %>%
+  group_by(taxon_name, trait_name, value_type) %>%
+  mutate(value = paste(value, collapse = " ")) %>%
+  ungroup() %>%
+  rename(value_new = value)
+
+counts_diff <- ABRS_old %>% 
+  distinct(taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>% 
+  left_join(ABRS_new) %>%
+  filter(is.na(value_new)) %>%
+  filter(!trait_name %in% c("woodiness", "woodiness_detailed", "life_history", "plant_growth_form")) %>%
+  group_by(trait_name) %>% mutate(n_taxa = n()) %>% ungroup() %>% distinct(trait_name, n_taxa) %>%
+  arrange(n_taxa)
+
+# retain_Trichosanthes_cucu_cucu <-
+#   austraits$traits %>% 
+#   filter(dataset_id == "ABRS_1981") %>% 
+#   filter(taxon_name == "Trichosanthes cucumerina var. cucumerina") %>% 
+#   select(taxon_name, trait_name, value_type, value, unit, original_name, observation_id)
+# 
+# 
+# retain_Synostemon_glaucus <-
+#   austraits$traits %>% 
+#   filter(dataset_id == "ABRS_1981") %>% 
+#   filter(taxon_name == "Synostemon glaucus") %>% 
+#   filter(trait_name == "flowering_time") %>%
+#   select(taxon_name, trait_name, value_type, value, unit, original_name, observation_id) 
+
+retain_1 <- ABRS_old %>%
+  distinct(taxon_name, trait_name, value_type, value, unit, .keep_all = TRUE) %>%
+  left_join(ABRS_new) %>%
+  filter(is.na(value_new)) %>%
+  filter(!trait_name %in% c("woodiness", "woodiness_detailed", "life_history", "plant_growth_form"))
+
+retain_missing_taxa <- retain_1 %>% filter(!taxon_name %in% ABRS_new$taxon_name)
+
+retain_2 <- retain_1 %>% anti_join(retain_missing_taxa)
+
+retain_trustworthy_traits <-
+  retain_2 %>%
+  filter(trait_name %in% c("leaf_phenology", "flowering_time", "leaf_compoundness", "dispersal_appendage", "plant_growth_substrate", "seed_shape"))
+      
+retain_seed_traits <-
+  retain_2 %>%
+  filter(trait_name %in% c("seed_length", "seed_width", "seed_height"))
+
+retain_3 <- retain_2 %>% anti_join(retain_trustworthy_traits) %>% anti_join(retain_seed_traits)
+
+new_max_seed_widths <- 
+  ABRS_new %>% 
+  filter(trait_name == "seed_width") %>%
+  filter(value_type == "maximum") %>%
+  select(-value_type) %>%
+  rename(value_new_max = value_new)
+
+retain_missing_seed_widths <- 
+  retain_2 %>% 
+  filter(trait_name == "seed_width") %>%
+  left_join(new_max_seed_widths) %>%
+  filter(is.na(value_new_max) | value != value_new_max) %>%
+  arrange(taxon_name, value_type) %>%
+  select(-value_new, -value_new_max) %>%
+  distinct(taxon_name, trait_name, value, unit, observation_id, original_name, .keep_all = TRUE) #remove min = max
+
+missing_seed_heights <-
+  retain_2 %>%
+  filter(trait_name == "seed_height")
+
+retain_missing_seed_heights <- 
+  ABRS_old %>%
+  filter(trait_name %in% c("seed_height", "seed_width")) %>%
+  select(taxon_name, trait_name, value_type, value, unit, observation_id, original_name) %>%
+  pivot_wider(names_from = trait_name, values_from = value) %>%
+  filter(seed_height != seed_width) %>%
+  filter(observation_id %in% missing_seed_heights$observation_id) %>%
+  rename(value = seed_height) %>%
+  select(-seed_width) %>%
+  mutate(trait_name = "seed_height") %>%
+  distinct(taxon_name, trait_name, value, unit, observation_id, original_name, .keep_all = TRUE) #remove min = max
+
+
+
+ABRS_retain <-
+  retain_missing_taxa %>% 
+  #bind_rows(retain_missing_taxa) %>%
+  bind_rows(retain_trustworthy_traits) #%>%
+#  bind_rows(retain_missing_seed_heights) %>%
+#  bind_rows(retain_missing_seed_widths) %>%
+  
+#    bind_rows(ABRS_old %>% filter(trait_name %in% c("seed_height", "seed_width")) %>% filter(taxon_name == "Acacia gonocarpa")) %>%
+#  bind_rows(ABRS_old %>% filter(trait_name %in% c("leaf_length", "leaf_width")) %>% filter(taxon_name %in% c("Nelumbo nucifera", "Ficus carpentariensis"))) %>%
+#  bind_rows(retain_Trichosanthes_cucu_cucu) %>%
+#  bind_rows(retain_Synostemon_glaucus) %>%
+#  filter(value != "yyyyynnyyyyy yyyyynnnyyyy") %>%
+#  filter(!trait_name %in% c("life_history", "plant_growth_form"))
+
+ABRS_retain %>% select(-value_new) %>% write_csv("data/ABRS_1981/data.csv")
+
+#
