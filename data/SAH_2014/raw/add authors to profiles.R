ee0d52fd55db47b8c6acaef9e2c52c5d59599a7a
Elizabeth Wenk <ehwenk@gmail.com>
2024-02-08 16:24:16 +1100

    add authors to SAH, NTH datasets

---
+++ data/SAH_2014/raw/add authors to profiles.R
@@ -0,0 +1,31 @@
+# extract authors from current NTH download
+
+read_csv("data/SAH_2014/raw/SA_raw.csv") %>%
+  select(taxon_name, author, citation) %>%
+  mutate(author = ifelse(author == "Author:Not yet available", NA, author)) %>%
+  filter(!(is.na(author) & is.na(citation))) %>%
+  mutate(
+    authors = ifelse(is.na(author), citation, NA),
+    authors = ifelse(is.na(citation), author, authors),
+    authors = ifelse(!is.na(citation) & !is.na(author), paste0(author, "; ", citation), authors)
+  ) %>%
+  select(taxon_name, authors) %>%
+  distinct() %>% 
+  write_csv("data/SAH_2014/raw/authors.csv")
+
+# manually remove special characters
+read_csv("data/SAH_2014/raw/authors.csv") -> authors
+
+# add authors to all 3 SAH dataset
+
+read_csv("data/SAH_2014/data.csv") %>%
+  left_join(authors) %>%
+  write_csv("data/SAH_2014/data.csv") 
+
+read_csv("data/SAH_2014/data.csv") %>%
+  left_join(authors) %>%
+  write_csv("data/SAH_2014/data.csv") 
+
+read_csv("data/SAH_2014/data.csv") %>%
+  left_join(authors) %>%
+  write_csv("data/SAH_2014/data.csv") 
